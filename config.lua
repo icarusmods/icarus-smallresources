85d7b44e3bb6f6899723dc34ec1df541c93cc5f8
icarusdevelopment12317 <maybeicarus@gmail.com>
2024-02-12 00:12:02 -0500

    1.3
    Added first person forced while shooting from car

--- config.lua
+++ config.lua
@@ -1,8 +1,19 @@
 Config = {}
-Config.bfdisable = true -- Disable blind-firing, removes blind firing behind cover
-Config.spdisable = true -- Disable spam punching, must be targeting player/ped to swing
-Config.crdisable = true -- Disable combat roll
-Config.ftdisable = true -- Disable flashlight turning off after un-aiming
-Config.amdisable = true -- Disable "action mode" (the stance after shooting or swinging)
-Config.hatgone = false -- Disable "action mode" (the stance after shooting or swinging)
--- Pause menu text
+
+Config.bfdisable = true 
+-- Disable blind-firing, removes blind firing behind cover
+
+Config.spdisable = true
+-- Disable spam punching, must be targeting player/ped to swing
+
+Config.crdisable = true 
+-- Disable combat roll
+
+Config.ftdisable = true 
+-- Disable flashlight turning off after un-aiming
+
+Config.amdisable = true 
+-- Disable "action mode" (the stance after shooting or swinging)
+
+Config.FPShooting = true 
+-- Forces first person while shooting from car
